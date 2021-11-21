5b6f8cb4b9417574e00db744ea0afc1b23923a86
Chitsanupong <first30952@gmail.com>
2021-11-21 21:34:29 +0700

    feat: apply swagger ui

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,22 @@
 import express, { Application, Request, Response } from "express"
 
+// swagger
+import swaggerUI from "swagger-ui-express"
+import swaggerSpec from "./configs/swagger"
+import swaggerStats from "swagger-stats"
+
 const PORT = process.env.PORT || 8080
 
 const app: Application = express()
+
+app.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerSpec, { explorer: true }))
+app.use(
+  swaggerStats.getMiddleware({
+    name: "mascot-stats",
+    uriPath: "/stats",
+    swaggerSpec: swaggerSpec,
+  })
+)
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!")
