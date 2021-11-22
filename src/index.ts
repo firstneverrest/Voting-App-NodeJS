e4bf7872c1eb832d5b6776b2ba65203513de7185
Chitsanupong <first30952@gmail.com>
2021-11-22 13:16:03 +0700

    feat: apply morgan & router

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,25 @@
 import express, { Application, Request, Response } from "express"
+import morgan from "morgan"
 
 // swagger
 import swaggerUI from "swagger-ui-express"
 import swaggerSpec from "./configs/swagger"
 import swaggerStats from "swagger-stats"
 
+// cors
+import cors from "cors"
+import CORS_OPTION from "./configs/cors"
+
+import apiRouter from "./routes/index"
+
 const PORT = process.env.PORT || 8080
 
 const app: Application = express()
 
+app.use(cors(CORS_OPTION))
+app.use(morgan("dev"))
+
+app.use("/api/v1", apiRouter)
 app.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerSpec, { explorer: true }))
 app.use(
   swaggerStats.getMiddleware({
